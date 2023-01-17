50d63e58637f79f5641a7579dceab2337c563244
ISungCho <chois9660@gmail.com>
2023-01-17 09:52:21 +0900

    feat(ch12): 복합패턴 - 데코레이터패턴

--- ch12/DuckSimulator.ts
+++ ch12/DuckSimulator.ts
@@ -1,13 +1,14 @@
 import { MallardDuck, Quackable, RedheadDuck, DuckCall, RubberDuck } from './Duck'
 import { Goose } from './Goose'
 import { GooseAdapter } from './GooseAdapter'
+import { QuakCounter } from './QuackCounter'
 
 export class DuckSimulator {
 	startSimulate() {
-		const mallardDuck = new MallardDuck()
-		const redheadDuck = new RedheadDuck()
-		const duckCall = new DuckCall()
-		const rubberDuck = new RubberDuck()
+		const mallardDuck = new QuakCounter(new MallardDuck())
+		const redheadDuck = new QuakCounter(new RedheadDuck())
+		const duckCall = new QuakCounter(new DuckCall())
+		const rubberDuck = new QuakCounter(new RubberDuck())
 		const goose = new Goose()
 
 		console.log('\n오리 시뮬레이션 게임')
@@ -15,7 +16,8 @@ export class DuckSimulator {
 		this.simulate(redheadDuck)
 		this.simulate(duckCall)
 		this.simulate(rubberDuck)
-		this.simulate(new GooseAdapter(goose))
+    this.simulate(new GooseAdapter(goose))
+    console.log(QuakCounter.getQuacks())
 	}
 
 	simulate(duck: Quackable) {
